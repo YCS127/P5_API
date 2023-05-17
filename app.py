92a3209dbc1a34aca16f417d9ac00d533d302af6
YCS127 <61731881+YCS127@users.noreply.github.com>
2023-05-17 21:02:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ from nltk.corpus import stopwords
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
 from nltk import word_tokenize
 from nltk.tokenize import sent_tokenize
 from nltk.stem import WordNetLemmatizer
